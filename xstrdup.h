9801671484f3620056a9d1d12b9a9ee09583f574
Nicholas J. Kain <nicholas@kain.us>
2014-03-20 05:56:47 -0400

    Add xstrdup.h.

---
+++ xstrdup.h
@@ -0,0 +1,16 @@
+#ifndef NCMLIB_XSTRDUP_H_
+#define NCMLIB_XSTRDUP_H_
+
+#include <string.h>
+#include "log.h"
+
+#define xstrdup(a) xstrdup_z_(a,__func__)
+static inline char *xstrdup_z_(const char *s, const char *fn)
+{
+    char *r = strdup(s);
+    if (!r)
+        suicide("%s: strdup failed", fn);
+    return r;
+}
+
+#endif /* NCMLIB_XSTRDUP_H_ */
