20139263e574d0422998d00bdcac7bc164f974c5
Nicholas J. Kain <nicholas@kain.us>
2014-03-30 14:57:09 -0400

    Rewrite the functions in cap.c and chroot.c.  Use setresuid/setresgid.
    For systems that lack setresuid/setresgid, I would rather write an
    emulation function to provide the proper semantics than wade through the
    semantic minefield of getuid/getgid and friends.
    
    The capability functions must have the NCM_USE_CAPABILITY symbol defined
    to be exposed.
    
    chroot.[ch] will be renamed in the next commit.

--- chroot.c
+++ chroot.c
@@ -26,49 +26,57 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
+#ifdef NCM_USE_CAPABILITY
+#include <sys/capability.h>
+#include <sys/prctl.h>
+#endif
 
 #include "chroot.h"
 #include "defines.h"
 #include "log.h"
 
-void imprison(const char *chroot_dir)
+void nk_set_chroot(const char *chroot_dir)
 {
-    if (chroot(chroot_dir)) {
-        log_error("%s: chroot('%s') failed: %s", __func__, chroot_dir,
-                  strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    if (chdir("/")) {
-        log_error("%s: chdir('/') failed: %s", __func__, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+    if (chroot(chroot_dir))
+        suicide("%s: chroot('%s') failed: %s", __func__, chroot_dir,
+                strerror(errno));
+    if (chdir("/"))
+        suicide("%s: chdir('/') failed: %s", __func__, strerror(errno));
 }
 
-void drop_root(uid_t uid, gid_t gid)
+void nk_set_uidgid(uid_t uid, gid_t gid)
 {
-    if (uid == 0 || gid == 0) {
-        log_error("%s: attempt to drop root to root", __func__);
-        exit(EXIT_FAILURE);
-    }
-
-    if (getgid() != gid) {
-        if (setgid(gid)) {
-            log_error("%s: setgid failed: %s", __func__, strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    if (getuid() != uid) {
-        if (setuid(uid)) {
-            log_error("%s: setuid failed: %s", __func__, strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    }
+    if (setgroups(1, &gid))
+        suicide("%s: setgroups failed: %s", __func__, strerror(errno));
+    if (setresgid(gid, gid, gid))
+        suicide("%s: setresgid failed: %s", __func__, strerror(errno));
+    if (setresuid(uid, uid, uid))
+        suicide("%s: setresuid failed: %s", __func__, strerror(errno));
+    if (getgid() != gid || getuid() != uid)
+        suicide("%s: failed because the OS or libc is broken", __func__);
 }
 
+#ifdef NCM_USE_CAPABILITY
+void nk_set_capability(const char *captxt)
+{
+    if (!captxt)
+        return;
+    if (prctl(PR_SET_KEEPCAPS, 1))
+        suicide("%s: prctl failed: %s", __func__, strerror(errno));
+    cap_t caps = cap_from_text(captxt);
+    if (!caps)
+        suicide("%s: cap_from_text failed: %s", __func__, strerror(errno));
+    if (cap_set_proc(caps))
+        suicide("%s: cap_set_proc failed: %s", __func__, strerror(errno));
+    if (cap_free(caps))
+        suicide("%s: cap_free failed: %s", __func__, strerror(errno));
+}
+#endif
